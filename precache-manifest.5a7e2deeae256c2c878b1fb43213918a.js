e1b893d2e2e5eef933490b63b88d7db8e21d4778
Alexander Teterin <sctothevision@gmail.com>
2022-01-11 20:32:59 +0300

    Add files via upload

---
+++ precache-manifest.5a7e2deeae256c2c878b1fb43213918a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ee63ce7dac9a104048c3ceac76ac8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c33f1a4fa357f3afee",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "c2c33f1a4fa357f3afee",
+    "url": "/static/js/main.bf975778.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
